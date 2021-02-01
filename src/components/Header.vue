9896044998d583de8c23344e8010c388ffadf753
E99p1ant <i@github.red>
2021-02-01 22:56:28 +0800

    V0.7.3 (#24)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,78 +1,83 @@
 <template>
-    <div>
-<!--        <TaskView :visible="taskVisible" @visibleChange="(val) => {taskVisible = val}"></TaskView>-->
-        <el-menu :default-active="this.$route.path" mode="horizontal" :router="true" background-color="#323232"
-                 text-color="#fff" active-text-color="#2194EC">
-            <el-menu-item>{{ base.Title }}</el-menu-item>
-            <el-menu-item index="/">{{$t('header.panel')}}</el-menu-item>
-            <el-menu-item index="/team">{{$t('header.team')}}</el-menu-item>
-            <el-menu-item index="/challenge">{{$t('header.challenge')}}</el-menu-item>
-            <el-menu-item index="/gamebox">{{$t('header.gamebox')}}</el-menu-item>
-<!--            <el-menu-item index="/docker">{{$t('header.docker')}}</el-menu-item>-->
-            <el-menu-item index="/flag">{{$t('header.flag')}}</el-menu-item>
-            <el-menu-item index="/bulletin">{{$t('header.bulletin')}}</el-menu-item>
-            <el-menu-item index="/asteroid">{{$t('header.asteroid')}}</el-menu-item>
-            <el-menu-item index="/webhook">{{$t('header.webhook')}}</el-menu-item>
-            <el-menu-item index="/config">{{$t('header.config')}}</el-menu-item>
-            <el-menu-item index="/manager">{{$t('header.manager')}}</el-menu-item>
-            <el-menu-item v-if="$route.name !== 'Login'" index="" @click="onLogout">{{$t('header.logout')}}
-            </el-menu-item>
-            <el-menu-item index="">
-                <el-dropdown @command="(lang) => this.$i18n.locale = lang" class="height: 100%;">
+  <div>
+    <!--        <TaskView :visible="taskVisible" @visibleChange="(val) => {taskVisible = val}"></TaskView>-->
+    <el-menu :default-active="this.$route.path" mode="horizontal" :router="true" background-color="#323232"
+             text-color="#fff" active-text-color="#2194EC">
+      <el-menu-item>{{ base.Title }}</el-menu-item>
+      <el-menu-item index="/">{{ $t('header.panel') }}</el-menu-item>
+      <el-menu-item index="/team">{{ $t('header.team') }}</el-menu-item>
+      <el-menu-item index="/challenge">{{ $t('header.challenge') }}</el-menu-item>
+      <el-menu-item index="/gamebox">{{ $t('header.gamebox') }}</el-menu-item>
+      <!--            <el-menu-item index="/docker">{{$t('header.docker')}}</el-menu-item>-->
+      <el-menu-item index="/flag">{{ $t('header.flag') }}</el-menu-item>
+      <el-menu-item index="/bulletin">{{ $t('header.bulletin') }}</el-menu-item>
+      <el-menu-item index="/asteroid">{{ $t('header.asteroid') }}</el-menu-item>
+      <el-menu-item index="/webhook">{{ $t('header.webhook') }}</el-menu-item>
+      <el-menu-item index="/config">{{ $t('header.config') }}</el-menu-item>
+      <el-menu-item index="/manager">{{ $t('header.manager') }}</el-menu-item>
+      <el-menu-item v-if="$route.name !== 'Login'" index="" @click="onLogout">{{ $t('header.logout') }}
+      </el-menu-item>
+      <el-menu-item index="">
+        <el-dropdown @command="(lang) => this.$i18n.locale = lang" class="height: 100%;">
                     <span>
-                        {{$i18n.locale}}
+                        {{ $i18n.locale }}
                     </span>
-                    <el-dropdown-menu slot="dropdown">
-                        <el-dropdown-item v-for="(lang, index) in $i18n.availableLocales" v-bind:key="index"
-                                          :command="lang">{{lang}}
-                        </el-dropdown-item>
-                    </el-dropdown-menu>
-                </el-dropdown>
-            </el-menu-item>
-<!--            <el-menu-item style="float: right;">-->
-<!--                <el-badge :value="200" :max="99" class="badge">-->
-<!--                    <el-button plain size="small" style="margin-top: -20px;" @click="taskVisible = true">任务队列-->
-<!--                    </el-button>-->
-<!--                </el-badge>-->
-<!--            </el-menu-item>-->
-        </el-menu>
-    </div>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item v-for="(lang, index) in $i18n.availableLocales" v-bind:key="index"
+                              :command="lang">{{ lang }}
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-menu-item>
+      <!--            <el-menu-item style="float: right;">-->
+      <!--                <el-badge :value="200" :max="99" class="badge">-->
+      <!--                    <el-button plain size="small" style="margin-top: -20px;" @click="taskVisible = true">任务队列-->
+      <!--                    </el-button>-->
+      <!--                </el-badge>-->
+      <!--            </el-menu-item>-->
+    </el-menu>
+  </div>
 </template>
 
 <script>
-    // import TaskView from "./TaskView";
+// import TaskView from "./TaskView";
 
-    export default {
-        name: "Header",
-        // components: {TaskView},
-        data: () => ({
-            base: {
-                Title: ''
-            },
-            taskVisible: false,
-        }),
+export default {
+  name: "Header",
+  // components: {TaskView},
+  data: () => ({
+    base: {
+      Title: ''
+    },
+    taskVisible: false,
+  }),
 
-        created() {
-            this.utils.GET('/base').then(res => {
-                this.base = res
-            }).catch(err => this.$message.error(err))
-        },
+  created() {
+    this.utils.GET('/base').then(res => {
+      this.base = res
+      this.setLanguage()
+    }).catch(err => this.$message.error(err))
+  },
 
-        methods: {
-            onLogout() {
-                this.utils.GET('/manager/logout').then(res => {
-                    this.$message.success(res)
-                    localStorage.removeItem('managerToken')
-                    this.$router.push('/login')
-                }).catch(err => this.$message.error(err))
-            },
-        },
-    }
+  methods: {
+    setLanguage() {
+      this.$i18n.locale = this.base.Language
+    },
+
+    onLogout() {
+      this.utils.GET('/manager/logout').then(res => {
+        this.$message.success(res)
+        localStorage.removeItem('managerToken')
+        this.$router.push('/login')
+      }).catch(err => this.$message.error(err))
+    },
+  },
+}
 </script>
 
 <style scoped>
-    .badge {
-        margin-top: 10px;
-        margin-right: 30px;
-    }
+.badge {
+  margin-top: 10px;
+  margin-right: 30px;
+}
 </style>
--- src/views/Flag.vue
+++ src/views/Flag.vue
@@ -1,168 +1,169 @@
 <template>
-    <div>
-        <el-form :inline="true">
-            <el-form-item>
-                <el-button type="primary" @click="generateFlag">{{$t('flag.generate_flag')}}</el-button>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="exportFlagDialogVisible = true">{{$t('flag.export_flag')}}</el-button>
-            </el-form-item>
-            <el-form-item></el-form-item>
-            <el-form-item label="轮数">
-                <el-input-number v-model="round" controls-position="right" :min="0" :max="10"></el-input-number>
-            </el-form-item>
-            <el-form-item label="队伍">
-                <el-select v-model="team" placeholder="选择队伍">
-                    <el-option
-                            v-for="item in teamList"
-                            :key="item.ID"
-                            :label="item.Name"
-                            :value="item.ID">
-                    </el-option>
-                </el-select>
-            </el-form-item>
-            <el-form-item label="题目">
-                <el-select v-model="challenge" placeholder="选择题目">
-                    <el-option
-                            v-for="item in challengeList"
-                            :key="item.ID"
-                            :label="item.Title"
-                            :value="item.ID">
-                    </el-option>
-                </el-select>
-            </el-form-item>
-            <el-form-item>
-                <el-button icon="el-icon-search" type="primary" round @click="()=> {page = 1; getFlags()}">筛选
-                </el-button>
-                <el-button round @click="cleanFilter">清除条件</el-button>
-            </el-form-item>
-        </el-form>
+  <div>
+    <el-form :inline="true">
+      <el-form-item>
+        <el-button type="primary" @click="generateFlag">{{ $t('flag.generate_flag') }}</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="exportFlagDialogVisible = true">{{ $t('flag.export_flag') }}</el-button>
+      </el-form-item>
+      <el-form-item></el-form-item>
+      <el-form-item :label="$t('flag.round')">
+        <el-input-number v-model="round" controls-position="right" :min="0" :max="10"></el-input-number>
+      </el-form-item>
+      <el-form-item :label="$t('flag.team')">
+        <el-select v-model="team" :placeholder="$t('flag.select_team')">
+          <el-option
+              v-for="item in teamList"
+              :key="item.ID"
+              :label="item.Name"
+              :value="item.ID">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item :label="$t('flag.challenge')">
+        <el-select v-model="challenge" :placeholder="$t('flag.select_challenge')">
+          <el-option
+              v-for="item in challengeList"
+              :key="item.ID"
+              :label="item.Title"
+              :value="item.ID">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button icon="el-icon-search" type="primary" round @click="()=> {page = 1; getFlags()}">
+          {{ $t('flag.filter') }}
+        </el-button>
+        <el-button round @click="cleanFilter"> {{ $t('flag.clean_filter') }}</el-button>
+      </el-form-item>
+    </el-form>
 
-        <el-table :data="flagList" style="width: 100%" stripe v-loading="flagList === null">
-            <el-table-column width="80" prop="ID" label="ID"/>
-            <el-table-column width="80" prop="TeamID" :label="$t('flag.team')"/>
-            <el-table-column width="80" prop="ChallengeID" :label="$t('flag.challenge')"/>
-            <el-table-column width="80" prop="GameBoxID" :label="$t('flag.gamebox')"/>
-            <el-table-column width="80" prop="Round" :label="$t('flag.round')"/>
-            <el-table-column prop="Flag" :label="$t('flag.flag')"/>
-        </el-table>
-        <br>
-        <el-pagination style="text-align:center" background layout="prev, pager, next" :total="total" :page-size="per"
-                       @current-change="(p)=>{page = p; getFlags()}"></el-pagination>
+    <el-table :data="flagList" style="width: 100%" stripe v-loading="flagList === null">
+      <el-table-column width="80" prop="ID" label="ID"/>
+      <el-table-column width="80" prop="TeamID" :label="$t('flag.team')"/>
+      <el-table-column width="80" prop="ChallengeID" :label="$t('flag.challenge')"/>
+      <el-table-column width="80" prop="GameBoxID" :label="$t('flag.gamebox')"/>
+      <el-table-column width="80" prop="Round" :label="$t('flag.round')"/>
+      <el-table-column prop="Flag" :label="$t('flag.flag')"/>
+    </el-table>
+    <br>
+    <el-pagination style="text-align:center" background layout="prev, pager, next" :total="total" :page-size="per"
+                   @current-change="(p)=>{page = p; getFlags()}"></el-pagination>
 
-        <!-- Export -->
-        <el-dialog :title="$t('flag.export_flag')" :visible.sync="exportFlagDialogVisible">
-            <el-form label-width="120px">
-                <el-form-item :label="$t('challenge.title')">
-                    <el-select v-model="exportChallengeID">
-                        <el-option
-                                v-for="challenge in challengeList"
-                                :key="challenge.ID"
-                                :label="challenge.Title"
-                                :value="challenge.ID">
-                        </el-option>
-                    </el-select>
-                </el-form-item>
-            </el-form>
-            <el-button type="primary" @click="exportFlag">{{$t('general.export')}}</el-button>
-        </el-dialog>
-    </div>
+    <!-- Export -->
+    <el-dialog :title="$t('flag.export_flag')" :visible.sync="exportFlagDialogVisible">
+      <el-form label-width="120px">
+        <el-form-item :label="$t('challenge.title')">
+          <el-select v-model="exportChallengeID">
+            <el-option
+                v-for="challenge in challengeList"
+                :key="challenge.ID"
+                :label="challenge.Title"
+                :value="challenge.ID">
+            </el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <el-button type="primary" @click="exportFlag">{{ $t('general.export') }}</el-button>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Flag",
+export default {
+  name: "Flag",
 
-        data: () => ({
-            page: 1,
-            per: 15,
+  data: () => ({
+    page: 1,
+    per: 15,
 
-            total: 0,
-            flagList: null,
+    total: 0,
+    flagList: null,
 
-            teamList: [],
-            challengeList: [],
-            exportFlagDialogVisible: false,
-            exportChallengeID: null,
+    teamList: [],
+    challengeList: [],
+    exportFlagDialogVisible: false,
+    exportChallengeID: null,
 
-            totalRound: 0,
+    totalRound: 0,
 
-            // Filter
-            round: 0,
-            team: null,
-            challenge: null,
-        }),
+    // Filter
+    round: 0,
+    team: null,
+    challenge: null,
+  }),
 
-        mounted() {
-            this.getTotalRound()
-            this.getTeams()
-            this.getFlags()
-            this.getChallenges()
-        },
+  mounted() {
+    this.getTotalRound()
+    this.getTeams()
+    this.getFlags()
+    this.getChallenges()
+  },
 
-        methods: {
-            getTeams() {
-                this.utils.GET("/manager/teams").then(res => {
-                    this.teamList = res
-                }).catch(err => this.$message.error(err))
-            },
+  methods: {
+    getTeams() {
+      this.utils.GET("/manager/teams").then(res => {
+        this.teamList = res
+      }).catch(err => this.$message.error(err))
+    },
 
-            getChallenges() {
-                this.utils.GET("/manager/challenges").then(res => {
-                    this.challengeList = res
-                }).catch(err => this.$message.error(err))
-            },
+    getChallenges() {
+      this.utils.GET("/manager/challenges").then(res => {
+        this.challengeList = res
+      }).catch(err => this.$message.error(err))
+    },
 
-            getTotalRound() {
-                this.utils.GET('/time').then(res => {
-                    this.totalRound = res.TotalRound
-                }).catch(err => this.$message({message: err, type: 'error'}))
-            },
+    getTotalRound() {
+      this.utils.GET('/time').then(res => {
+        this.totalRound = res.TotalRound
+      }).catch(err => this.$message({message: err, type: 'error'}))
+    },
 
-            getFlags() {
-                let team = this.team || 0
-                let challenge = this.challenge || 0
+    getFlags() {
+      let team = this.team || 0
+      let challenge = this.challenge || 0
 
-                this.utils.GET(`/manager/flags?per=${this.per}&page=${this.page}&round=${this.round}&team=${team}&challenge=${challenge}`).then(res => {
-                    this.flagList = res.array
-                    this.total = res.total
-                }).catch(err => this.$message({message: err, type: 'error'}))
-            },
+      this.utils.GET(`/manager/flags?per=${this.per}&page=${this.page}&round=${this.round}&team=${team}&challenge=${challenge}`).then(res => {
+        this.flagList = res.array
+        this.total = res.total
+      }).catch(err => this.$message({message: err, type: 'error'}))
+    },
 
-            cleanFilter() {
-                this.round = 0
-                this.team = null
-                this.challenge = null
-                this.page = 1
-                this.getFlags()
-            },
+    cleanFilter() {
+      this.round = 0
+      this.team = null
+      this.challenge = null
+      this.page = 1
+      this.getFlags()
+    },
 
-            generateFlag() {
-                this.$confirm(this.$i18n.t('flag.generate_flag_confirm'), this.$i18n.t('flag.confirm'), {
-                    confirmButtonText: this.$i18n.t('general.apply'),
-                    cancelButtonText: this.$i18n.t('general.cancel'),
-                    type: 'warning'
-                }).then(() => {
-                    this.utils.POST('/manager/flag/generate').then(res => {
-                        this.getFlags()
-                        this.$message({message: res, type: 'success'})
-                    }).catch(err => this.$message({message: err, type: 'error'}))
-                })
-            },
+    generateFlag() {
+      this.$confirm(this.$i18n.t('flag.generate_flag_confirm'), this.$i18n.t('flag.confirm'), {
+        confirmButtonText: this.$i18n.t('general.apply'),
+        cancelButtonText: this.$i18n.t('general.cancel'),
+        type: 'warning'
+      }).then(() => {
+        this.utils.POST('/manager/flag/generate').then(res => {
+          this.getFlags()
+          this.$message({message: res, type: 'success'})
+        }).catch(err => this.$message({message: err, type: 'error'}))
+      })
+    },
 
-            exportFlag() {
-                this.utils.GET("/manager/flag/export?id=" + this.exportChallengeID).then(res => {
-                    let blob = new Blob([JSON.stringify(res)], {type: 'text/plain'})
-                    let downloadElement = document.createElement("a")
-                    let href = window.URL.createObjectURL(blob)
-                    downloadElement.href = href
-                    downloadElement.download = name
-                    downloadElement.click()
-                    window.URL.revokeObjectURL(href)
-                }).catch(err => this.$message({message: err, type: 'error'}))
-            }
-        }
+    exportFlag() {
+      this.utils.GET("/manager/flag/export?id=" + this.exportChallengeID).then(res => {
+        let blob = new Blob([JSON.stringify(res)], {type: 'text/plain'})
+        let downloadElement = document.createElement("a")
+        let href = window.URL.createObjectURL(blob)
+        downloadElement.href = href
+        downloadElement.download = name
+        downloadElement.click()
+        window.URL.revokeObjectURL(href)
+      }).catch(err => this.$message({message: err, type: 'error'}))
     }
+  }
+}
 </script>
 
 <style scoped>
